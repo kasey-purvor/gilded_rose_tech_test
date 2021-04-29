70a4ed90a58fc0a25fcaee52040eb6b72ca90f1e
kasey-purvor <kasey.purvor@gmail.com>
2021-04-29 13:29:28 +0100

    refactored methods to avoid repetition and extract functionality out of 'update' methods

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -9,58 +9,129 @@ class Item {
 class Shop {
   constructor(items=[]){
     this.items = items;
-  }
+  };
   
+  isARegularItem(i) {
+    if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Conjured Item') {
+      return true;
+    };
+  }; 
+
+  isOutOfDate(i) {
+    if (this.items[i].sellIn <= 0) {
+      return true;
+    };
+  };
+
+  isNotOutOfDate(i) {
+    if (this.items[i].sellIn > 0) {
+      return true;
+    };
+  };
+
+  hasQualityBelowZero(i) {
+    if (this.items[i].quality < 0) {
+      return true;
+    };
+  };
+
   regularItemUpdate(i) {
-    if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Conjured Item' && this.items[i].sellIn <= 0) {
+    if (this.isARegularItem(i) && this.isOutOfDate(i)) {
       this.items[i].quality = this.items[i].quality - 2;
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Conjured Item' && this.items[i].sellIn > 0) {
+    } if (this.isARegularItem(i) && this.isNotOutOfDate(i)) {
       this.items[i].quality = this.items[i].quality - 1;
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Conjured Item' && this.items[i].quality < 0){
+    } if (this.isARegularItem(i) && this.hasQualityBelowZero(i)) {
       this.items[i].quality = 0;
-    }
+    };
+  };
+
+  isAConjuredItem(i) {
+    if(this.items[i].name === 'Conjured Item') {
+      return true;
+    };
   };
 
   conjuredItemUpdate(i) {
-    if (this.items[i].name === 'Conjured Item' && this.items[i].sellIn <= 0) {
+    if (this.isAConjuredItem(i) && this.isOutOfDate(i)) {
       this.items[i].quality = this.items[i].quality - 4;
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if (this.items[i].name === 'Conjured Item' && this.items[i].sellIn > 0) {
+    } if (this.isAConjuredItem(i) && this.isNotOutOfDate(i)) {
       this.items[i].quality = this.items[i].quality - 2;
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if (this.items[i].name === 'Conjured Item' && this.items[i].quality < 0){
+    } if (this.isAConjuredItem(i) && this.hasQualityBelowZero(i)){
       this.items[i].quality = 0;
     }
   };
 
+  isAgedBrie(i) {
+    if (this.items[i].name === "Aged Brie") {
+      return true;
+    };
+  };
+
+  hasQualityAboveFifty(i) {
+    if (this.items[i].quality > 50) {
+      return true;
+    };
+  };
+
   brieUpdate(i) {
-    if (this.items[i].name === "Aged Brie" && this.items[i].sellIn <= 0){
+    if (this.isAgedBrie(i) && this.isOutOfDate(i)) {
       this.items[i].quality = this.items[i].quality + 2; 
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if(this.items[i].name === "Aged Brie" && this.items[i].sellIn > 0){
+    } if(this.isAgedBrie(i) && this.isNotOutOfDate(i) > 0){
       this.items[i].quality = this.items[i].quality + 1; 
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if(this.items[i].name === "Aged Brie" && this.items[i].quality > 50){
+    } if(this.isAgedBrie(i) && this.hasQualityAboveFifty(i)) {
       this.items[i].quality = 50 ;
-    }
+    };
   };
  
+  isATicket(i) {
+    if(this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+      return true;
+    };
+  };
+
+  hasLessThanSixDaysLeft(i) {
+    if(this.items[i].sellIn < 6) {
+      return true;
+    };
+  };
+  hasLessThanElevenDaysLeft(i) {
+    if(this.items[i].sellIn < 11) {
+      return true;
+    };
+  };
+
+  hasSixOrMoreDaysLeft(i) {
+    if(this.items[i].sellIn >= 6) {
+      return true;
+    };
+  };
+
+  hasElevenOrMoreDaysLeft(i) {
+    if(this.items[i].sellIn >= 11) {
+      return true;
+    };
+  };
+
   ticketsUpdate(i){
-    if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn <= 0) {
+    if (this.isATicket(i) && this.isOutOfDate(i)) {
       this.items[i].quality = 0
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn < 6 && this.items[i].sellIn > 0) {
+    } if (this.isATicket(i) && this.hasLessThanSixDaysLeft(i) && this.isNotOutOfDate(i)) {
       this.items[i].quality = this.items[i].quality + 3;
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn < 11 && this.items[i].sellIn >= 6) {
+    } if (this.isATicket(i) && this.hasLessThanElevenDaysLeft(i) && this.hasSixOrMoreDaysLeft(i)) {
       this.items[i].quality = this.items[i].quality + 2;
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn >= 11) {
+    } if (this.isATicket(i) && this.hasElevenOrMoreDaysLeft(i)) {
       this.items[i].quality = this.items[i].quality + 1;
       this.items[i].sellIn = this.items[i].sellIn - 1; 
-    } if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].quality > 50){
+    } if (this.isATicket(i) && this.hasQualityAboveFifty(i)) {
       this.items[i].quality = 50
     }
   };
